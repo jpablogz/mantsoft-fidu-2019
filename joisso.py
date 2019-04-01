3e5fe9e5c6422e20331cb9b46f71891787ad3326
Josue Solano <josuesolano.morales@gmail.com>
2019-04-01 14:17:30 -0600

    Trabajo 2

---
+++ joisso.py
@@ -0,0 +1,27 @@
+def darmensaje(mensaje):
+    print("Hola mundo!")
+    print(mensaje)
+
+darmensaje("dar mensaje")
+
+///////////////////////////////////////////////////////////
+
+
+#Trabajo numero II
+
+def PatternCount(Texto, Pattern):
+
+    count = 0
+
+    for i in range(len(Texto)):
+
+        if Texto[i:i+len(Pattern)] == Pattern: 
+            count+=1
+    return count
+
+Texto = "GCGCGCG"
+Pattern = "GCG"
+print(PatternCount(Texto, Pattern))
+
+"El IF busca el valor del índice i en el texto es igual al índice 0 en el patrón (que es G) al encontrar G en el texto, validara el indice del índice i al índice i + la longitud del patrón y ver si conincide o no el patron 
+
