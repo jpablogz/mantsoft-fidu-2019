e06efae64ac16d4c03505b8e1db9757aa8828bcf
Javier <rojasjavi@gmail.com>
2019-04-09 17:46:40 -0600

    Update jrh8.py
    Función PatternCount de Javier Rojas

--- jrh8.py
+++ jrh8.py
@@ -1,4 +1,25 @@
 def darmensaje(mensaje):
     print("Hola Mundo" + mensaje)
 
-darmensaje("es mi primera vez en python")
+darmensaje(" es mi primera vez en python ")
+
+
+#crear una funcion PatternCount que cuenta cuántas veces aparece
+#un patrón Pattern en una cadena de caracteres Text
+
+print("Buenas por favor ingrese un texto donde se repita un patrón y luego ingresa el patrón")
+
+def PatternCoun(text, pattern):
+    cont = 0
+    for i in range(0, len(text) - len(pattern) + 1):
+        if text[i : len(pattern) + i] == pattern:
+            cont += 1
+    return print ("El patrón se repite "+str(cont) + " veces en el texto")
+
+PatternCoun(input(), input());
+
+
+
+    
+        
+    
