450d593d2e740c8b886d1bd09d55962a6cf77cfd
jorgeom17 <48833319+jorgeom17@users.noreply.github.com>
2019-04-09 20:12:53 -0600

    Update jorgeom17.py
    Se añade una funcion para obtener la cantidad de veces que aparece una cadena en otra cadena. Entrega de semana 2

--- jorgeom17.py
+++ jorgeom17.py
@@ -1,6 +1,26 @@
+#PRIMERA ENTREGA
 #Funcion para imprimir mensaje
 def darmensaje (elMensaje):
     print('Hola Mundo! ' + elMensaje)
     return
 
 darmensaje('Jorge Ortiz Monge')
+
+#SEGUNDA ENTREGA
+#Funcion que obtiene la cantidad de veces que se repite una subcadena de caracteres en una cadena de caracteres
+#Recibe como parametros la cadena original y la cadena a buscar
+def PatternCount (pattern, text):
+    contador = 0
+
+    for indice in range(0, len(text)):
+        if (indice + len(pattern) > len(text)):
+            break
+        else:
+            if pattern == text[indice:indice + len(pattern)]:
+                contador = contador + 1
+
+    print(contador)
+
+    return
+
+PatternCount('GCG', 'GCGCG')
