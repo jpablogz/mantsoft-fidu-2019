ab5c99da4409c5594f528e31ff5d13b74f76b4a9
fertico21 <fertico21chm@gmail.com>
2019-04-11 12:17:24 -0600

    Update fertico21.py

--- fertico21.py
+++ fertico21.py
@@ -1,13 +1,13 @@
 
-# Esta función concatena 2 cádenas de texto.
-# Recibe como parametro un mensaje y retorna "Hola mundo" + mensaje enviado.
+# Esta función concatena 2 cádenas de texto
+# Recibe como parametro un mensaje y retorna "Hola mundo" + mensaje enviado
 def darmensaje(mensaje): 
     print ("Hola mundo!", mensaje)
 
-darmensaje("Fernando") 
+darmensaje("Fernando")
 
-# Esta Función determina la frecuencia con la que un patrón aparece en una cadena de texto.
-# Recibe como parámetros el texto y el patrón. Y retorna el número de veces que se encuentra ese patrón en el texto.
+# Esta Función determina la frecuencia con la que un patrón aparece en una cadena de texto
+# Recibe como parámetros el texto y el patrón. Y retorna el número de veces que se encuentra ese patrón en el texto
 def PatternCount(texto, patron):
     contador = 0
     for x in range(0, len(texto)):
@@ -15,5 +15,6 @@ def PatternCount(texto, patron):
             contador += 1
     return contador
 
-a = PatternCount("ADNABCDEFGHIJKLMNOPQRSTUWXYZADNABCDEFGHIJKLMNOPQRSTUWXYZADNABCDEFGHIJKLMNOPQRSTUWXYZADNADN","ADN")
+a = PatternCount("ADNABCDEFGHIJKLMNOPQRSTUWXYZADNABCDEFGHIJKLMNOPQRSTUWADNXYZADNABCDEFGHIJKLMNOPQRSTUWXYZADNADN","ADN")
 print (a)
+
