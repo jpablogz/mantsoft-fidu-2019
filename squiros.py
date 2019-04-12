b3993c7aea2506a7a2c9178d30b6d775898907d4
sofia <sofymabi@hotmail.es>
2019-04-11 18:35:14 -0600

    Update squiros.py
    Entrega de semana 2

--- squiros.py
+++ squiros.py
@@ -4,3 +4,23 @@ def darmensaje (elMensaje):
     return
 
 darmensaje('Sofía Quirós Ramírez')
+
+#Funcion para contar el número de veces que se repite una cadena en una subcadena
+
+def PatternCount (pattern, text):
+    contador = 0
+
+    for indice in range(0, len(text)):
+        if (indice + len(pattern) > len(text)):
+            break
+        else:
+            if pattern == text[indice:indice + len(pattern)]:
+                contador = contador + 1
+
+    print(contador)
+
+    return
+
+PatternCount('GCG', 'GCGCG')
+
+
